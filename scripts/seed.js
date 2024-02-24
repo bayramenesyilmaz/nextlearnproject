cc71f35983cc87377b9a25d473f51a665d4c6a17
Bayram Enes YILMAZ <63585653+bayramenesyilmaz@users.noreply.github.com>
2024-02-24 13:18:32 +0300

    database commit

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,4 +1,4 @@
-const { db} = require("@vercel/postgres");
+const { db } = require('@vercel/postgres');
 
 const {
   invoices,
@@ -162,7 +162,14 @@ async function seedRevenue(client) {
 }
 
 async function main() {
-  const client = await db.connect();
+  const client = await db.connect({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
+
+  });
+  console.log(client, "client");
 
   await seedUsers(client);
   await seedCustomers(client);
