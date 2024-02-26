76071779ecb40fb73a0b9a7cd24b39ef678a1879
Bayram Enes YILMAZ <63585653+bayramenesyilmaz@users.noreply.github.com>
2024-02-27 00:24:20 +0300

    style updated

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,9 @@ export default function Page() {
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
-        <div
+        {/* <div
           className={styles.shape}
-        />
+        /> */}
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
